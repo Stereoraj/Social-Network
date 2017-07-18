98c9e84f3d7923d471aac8bbc6ee782a16647ef0
rajesh <merajesh04@gmail.com>
2017-07-18 16:27:20 +0530

    the basic account-creation module

---
+++ create-account.php
@@ -0,0 +1,51 @@
+<?php
+	
+	include('classes/DB.php');
+
+	if(isset($_POST["submit"])){
+
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+		$email = $_POST["email"];
+
+		if(!DB::query("SELECT username FROM users WHERE username=:username",array(':username'=>$username))){
+
+			DB::query("INSERT INTO users(username,password,email) VALUES(:username,:password,:email)",array(':username'=>$username,':password'=>$password,':email'=>$email));
+
+			echo "successfully stored in the database !!";
+
+		}else{
+
+			echo "username already exists !!";
+		}
+
+	
+	}
+
+?>
+
+<h1>Register</h1>
+
+<form action="create-account.php" method="Post">
+	<table>
+		<tr>
+			<td>Name</td> 
+			<td><input type="text" name="username"></td>
+		</tr>
+
+		<tr>
+			<td>Password</td> 
+			<td><input type="password" name="password"></td>
+		</tr>
+
+		<tr>
+			<td>Email</td> 
+			<td><input type="email" name="email"></td>
+		</tr>
+		
+		<tr>
+			<td><input type="submit" name="submit" value="sign up"></td>
+		</tr>
+	</table>
+	
+</form>
