6660e12014397b952df90a898a8282478ccb6c9d
rajesh <merajesh04@gmail.com>
2017-08-27 11:59:00 +0530

    authenticate the user credential in login page

--- login.php
+++ login.php
@@ -7,9 +7,24 @@
 
 
     if(DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))){
+            // authenticating the user credentials
+
+            /* echo '<pre />';
+            var_dump(DB::query('SELECT * FROM users WHERE username=:username', array(':username'=>$username)));
+            echo '<pre />';
+
+            echo DB::query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'];
+            */
+
+            var_dump(password_verify($password,DB::query('SELECT password FROM users WHERE username=:username',array(':username'=>$username))[0]['password']));
+            if(password_verify($password,DB::query('SELECT password FROM users WHERE username=:username',array(':username'=>$username))[0]['password'])){
+                    echo 'valid password';
+            }else{
+                    echo 'invalid password';
+            }
 
     }else{
-      echo 'user not registered !!';
+            echo 'user not registered !!';
     }
 
   }
